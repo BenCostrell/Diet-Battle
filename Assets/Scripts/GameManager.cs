48f6fee22a272fc979de603f1290e9af07567c4b
Ben Costrell <bencostrell@gmail.com>
2017-02-15 14:35:06 -0500

    shoot plates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
 	public GameObject playerPrefab;
+	public GameObject platePrefab;
 	public Vector3 spawnP1;
 	public Vector3 spawnP2;
 
@@ -15,18 +17,21 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown ("Reset")) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
 	}
 
 	void InitializePlayers(){
-		GameObject player1 = Instantiate (playerPrefab, spawnP1, Quaternion.identity) as GameObject;
-		PlayerController pc1 = player1.GetComponent<PlayerController> ();
-		pc1.playerNum = 1;
-		player1.GetComponent<SpriteRenderer> ().color = Color.red;
+		InitializePlayer (spawnP1, 1, Color.red);
+		InitializePlayer (spawnP2, 2, Color.blue);
+	}
 
-		GameObject player2 = Instantiate (playerPrefab, spawnP2, Quaternion.identity) as GameObject;
-		PlayerController pc2 = player2.GetComponent<PlayerController> ();
-		pc2.playerNum = 2;
-		player2.GetComponent<SpriteRenderer> ().color = Color.blue;
+	void InitializePlayer(Vector3 spawnLocation, int playerNum, Color spriteColor){
+		GameObject player = Instantiate (playerPrefab, spawnLocation, Quaternion.identity) as GameObject;
+		PlayerController pc = player.GetComponent<PlayerController> ();
+		pc.playerNum = playerNum;
+		player.GetComponent<SpriteRenderer> ().color = spriteColor;
+		pc.platePrefab = platePrefab;
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,22 @@ public class PlayerController : MonoBehaviour {
 	public int playerNum;
 	public float speed;
 	private Rigidbody2D rb;
+	public GameObject platePrefab;
+	public float plateOffset;
+	public float plateSpeed;
+	public int platesInHolster;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();	
+		platesInHolster = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown ("Shoot_P" + playerNum) && (platesInHolster > 0)) {
+			ShootPlate ();
+		}
 	}
 
 	void FixedUpdate() {
@@ -30,4 +37,24 @@ public class PlayerController : MonoBehaviour {
 		float angle = Mathf.Atan2 (y, x);
 		transform.rotation = Quaternion.Euler (0, 0, angle * Mathf.Rad2Deg);
 	}
+
+	void ShootPlate(){
+		platesInHolster -= 1;
+		float angle = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+		Vector3 rotationVector = new Vector3 (Mathf.Cos (angle), Mathf.Sin (angle), 0);
+		Debug.Log (rotationVector);
+		GameObject plate = Instantiate (platePrefab, transform.position + (rotationVector * plateOffset), Quaternion.identity);
+		plate.GetComponent<Rigidbody2D> ().velocity = plateSpeed * rotationVector;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		GameObject go = collider.gameObject;
+		if (go.tag == "Plate") {
+			if (platesInHolster < 2) {
+				platesInHolster += 1;
+				Destroy (go);
+			}
+		}
+	}
+
 }
