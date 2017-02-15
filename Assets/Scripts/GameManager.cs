7a56349772f10ff87c4d8b0e172c707596fb6836
Ben Costrell <bencostrell@gmail.com>
2017-02-15 13:38:06 -0500

    basic player instantiation, movement, rotation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject playerPrefab;
+	public Vector3 spawnP1;
+	public Vector3 spawnP2;
+
+	// Use this for initialization
+	void Start () {
+		InitializePlayers ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitializePlayers(){
+		GameObject player1 = Instantiate (playerPrefab, spawnP1, Quaternion.identity) as GameObject;
+		PlayerController pc1 = player1.GetComponent<PlayerController> ();
+		pc1.playerNum = 1;
+		player1.GetComponent<SpriteRenderer> ().color = Color.red;
+
+		GameObject player2 = Instantiate (playerPrefab, spawnP2, Quaternion.identity) as GameObject;
+		PlayerController pc2 = player2.GetComponent<PlayerController> ();
+		pc2.playerNum = 2;
+		player2.GetComponent<SpriteRenderer> ().color = Color.blue;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int playerNum;
+	public float speed;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		Move ();
+	}
+
+	void Move(){
+		float y = Input.GetAxis ("Vertical_P" + playerNum);
+		float x = Input.GetAxis ("Horizontal_P" + playerNum);
+		Vector2 inputDirection = new Vector2 (x, y);
+		rb.velocity = speed * inputDirection;
+		float angle = Mathf.Atan2 (y, x);
+		transform.rotation = Quaternion.Euler (0, 0, angle * Mathf.Rad2Deg);
+	}
+}
